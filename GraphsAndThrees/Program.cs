dae1aedcc4faf4b16c4d907617d453d69100d69c
djanerch <96980908+djanerch@users.noreply.github.com>
2023-02-20 15:05:29 +0200

    write dfs algorithm

---
+++ GraphsAndThrees/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphsAndThrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TreeNode> list = new List<TreeNode>()
+            {
+                new TreeNode(4, 
+                new List<TreeNode>()
+                {
+                    new TreeNode(5),
+                    new TreeNode(5),
+                    new TreeNode(3,
+                    new List<TreeNode>()
+                    {
+                        new TreeNode(1),
+                        new TreeNode(1),
+                        new TreeNode(9)
+                    }
+                    )
+                }
+                )
+            };
+
+            TreeNode tree = new TreeNode(10, list);
+
+            Console.WriteLine(tree.CountOfElementViaDFS(9));
+        }
+    }
+}
---
+++ GraphsAndThrees/TreeNode.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace GraphsAndThrees
+{
+    public class TreeNode
+    {
+        protected TreeNode()
+        {
+        }
+        public TreeNode(int value)
+        {
+            Value = value;
+        }
+        public TreeNode(int value, List<TreeNode> children)
+        {
+            Value = value;
+            Children = children;
+        }
+
+        public int Value { get; private set; }
+        public List<TreeNode> Children { get; private set; }
+
+        public int CountOfElementViaDFS(int findedValue)
+        {
+            int count = 0;
+            if (this.Children != null)
+            {
+                foreach (var item in this.Children)
+                {
+                    if (item.Value == findedValue)
+                    {
+                        count++;
+                    }
+                    count += item.CountOfElementViaDFS(findedValue);
+                }
+            }
+            return count;
+        }
+    }
+}
